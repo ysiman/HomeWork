327dcdf50b7a1650b5b01e4b937ca21de7e0c670
HOME <threadoff@gmail.com>
2017-12-31 15:16:04 +0300

    Internet_311217

--- src/main/java/TradePack/Internet.java
+++ src/main/java/TradePack/Internet.java
@@ -6,7 +6,6 @@ import java.net.URL;
  * Created by Yanis on 26.12.2017.
  */
 public class Internet {
-
     public static String getHttpInfo(String linkStr){
         URL url = JsonUtils.createUrl(linkStr);
         String httpInf = JsonUtils.parseUrl(url);
